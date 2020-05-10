57a7d016b0c72af03f749e5f374e5c775fb4e226
Christina Giddens <giddens.christina@gmail.com>
2020-05-10 19:08:19 -0400

    MVP

--- src/App.test.js
+++ src/App.test.js
@@ -10,7 +10,7 @@ describe('App', () => {
     wrapper = rtl.render(<App />);
   });
 
-  it('displays the correct name starting with the API data', async () => {
+  test('displays the correct name starting with the API data', async () => {
     const text = await wrapper.findByText('Select a season');
     expect(text).toBeInTheDocument();
     expect(text).toBeVisible();
